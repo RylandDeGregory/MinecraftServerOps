04c9cde41a488f35c05a6e672b704a2561009a19
Ryland DeGregory <rylanddegregory@me.com>
2022-04-05 22:22:33 -0400

    set values as strings

--- UpdateDNS/run.ps1
+++ UpdateDNS/run.ps1
@@ -1,9 +1,9 @@
 param($eventGridEvent, $TriggerMetadata)
 
 # Make sure to pass hashtables to Out-String so they're logged correctly
-($eventGridEvent).Data.Authorization.Evidence | Write-Host
+($eventGridEvent).Data.Authorization.Evidence | Out-String | Write-Host
 
-($eventGridEvent).Data | Write-Host
+($eventGridEvent).Data | Out-String | Write-Host
 
 # Log request to Azure Storage Table
 # Push-OutputBinding -Name outputTable -Value @{
